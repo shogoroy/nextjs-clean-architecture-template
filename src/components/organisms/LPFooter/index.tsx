a4dcdd4b1d8bb5313c09658e12555723c4b4918f
Shogoro Yoshida <shogoro.yoshida@gmail.com>
2022-08-21 18:20:29 +0900

    💄 Update footer styles

--- src/components/organisms/LPFooter/index.tsx
+++ src/components/organisms/LPFooter/index.tsx
@@ -1,14 +1,13 @@
+import clsx from "clsx";
 import Image from "src/components/atoms/Image";
 
 interface Props {}
 
 const LPFooter: React.FC = () => {
   return (
-    <>
-      <footer className={"bg-black"}>
-        &copy; <Image src="/logo.svg" alt="Logo" width={72} height={16} />
-      </footer>
-    </>
+    <footer className={clsx("fixed", "b-0", "bg-black", "h-16")}>
+      &copy; <Image src="/logo.svg" alt="Logo" width={72} height={16} />
+    </footer>
   );
 };
 
