eeb3bead40c3b8a54868cfa3582002c29338ed9a
Shogoro Yoshida <shogoro.yoshida@gmail.com>
2022-08-21 22:12:09 +0900

    🔧 Update lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,23 @@
 module.exports = {
   extends: [
     "next/core-web-vitals",
+    "eslint:recommended",
     "plugin:prettier/recommended",
     "plugin:import/recommended",
+    "plugin:import/typescript",
   ],
   plugins: ["unused-imports"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+  },
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true,
+    },
+  },
+  globals: {
+    React: true,
+  },
 };
